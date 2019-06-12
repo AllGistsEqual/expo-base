f5ca4ceb2d13bc345d44ca3a9d9c1683449d9cb5
Konrad Abe <mail@allbitsequal.com>
2019-06-12 09:24:08 +0200

    added redux base and core middleware setup

--- App.jsx
+++ App.jsx
@@ -3,9 +3,13 @@ import {
     createAppContainer,
     createBottomTabNavigator
 } from 'react-navigation'
+import { Provider } from 'react-redux'
+import configureStore from './src/redux/store'
 import SceneHome from "./src/scenes/SceneHome"
 import SceneStart from "./src/scenes/SceneStart"
 import SceneSettings from "./src/scenes/SceneSettings"
+
+const store = configureStore()
 
 let TabNavigator = createBottomTabNavigator(
   {
@@ -22,6 +26,8 @@ let Navigation = createAppContainer(TabNavigator)
 
 export default function App() {
   return (
-    <Navigation />
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
   )
 }
---
+++ src/redux/reducers/application.reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    status: false,
+}
+
+const applicationReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default: {
+            return state
+        }
+    }
+}
+
+export default applicationReducer
